d7103b9a4b7517545936dcbeb006dde3f3d478cd
Lewis Howles <lewishowles@users.noreply.github.com>
2025-02-02 15:11:50 +0000

    Update the placeholder format

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
 
 export default defineConfig({
-	base: "[[BASE_URL]]",
+	base: "{{BASE_URL}}",
 	plugins: [
 		vue(),
 		vueDevTools(),
