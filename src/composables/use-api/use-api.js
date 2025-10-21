c7bba11cd653ebc16772502b389cbecd1386445f
Lewis Howles <lewishowles@users.noreply.github.com>
2025-10-21 13:20:51 +0100

    Add baseURL and GET query parameter options

--- src/composables/use-api/use-api.js
+++ src/composables/use-api/use-api.js
@@ -1,3 +1,4 @@
+import { get as getPropertyValue } from "@lewishowles/helpers/object";
 import { getFriendlyDisplay } from "@lewishowles/helpers/general";
 import { isNonEmptyString } from "@lewishowles/helpers/string";
 import { ref } from "vue";
@@ -6,28 +7,32 @@ import { ref } from "vue";
  * Simplify the process of making API calls by removing some of the boilerplate.
  */
 export default function useApi() {
+	// Our base URL, which is prepended to API calls. We do this so that the
+	// rest of the app doesn't have to think about where data comes from.
+	let baseUrl = "http://localhost:3000/api";
+
 	// Whether data is currently loading.
 	const isLoading = ref(false);
 	// Whether data has loaded successfully.
 	const isReady = ref(false);
 
 	/**
-	 * Perform a GET request to the provided URL.
+	 * Perform a GET request to the provided endpoint, combined with a known
+	 * base path..
 	 *
-	 * @param  {string}  url
-	 *     The URL from which to load data.
+	 * @param  {string}  endpoint
+	 *     The endpoint from which to load data.
+	 * @param  {object}  parameters
+	 *     Any parameters to add as a query string.
 	 */
-	async function get(url) {
+	async function get(endpoint, parameters) {
 		try {
 			isLoading.value = true;
 
-			if (!isNonEmptyString(url)) {
-				throw new Error(`Expected non-empty string <url>, received ${getFriendlyDisplay(url)}`);
-			}
+			let response = await fetch(getFinalUrl(endpoint, parameters));
 
-			let response = await fetch(url);
-
-			response = response.json();
+			response = await response.json();
+			response = getPropertyValue(response, "data");
 
 			isReady.value = true;
 
@@ -37,9 +42,56 @@ export default function useApi() {
 		}
 	}
 
+	/**
+	 * Combine the provided endpoint and the base URL to create our final URL to
+	 * call.
+	 *
+	 * @param  {string}  endpoint
+	 *     The endpoint for a single call, to be appended to the base URL.
+	 * @param  {object}  parameters
+	 *     Any parameters to add as a query string.
+	 */
+	function getFinalUrl(endpoint, parameters) {
+		if (!isNonEmptyString(baseUrl)) {
+			throw new Error(`Expected non-empty string <baseUrl>, received ${getFriendlyDisplay(baseUrl)}`);
+		}
+
+		if (!isNonEmptyString(endpoint)) {
+			throw new Error(`Expected non-empty string <endpoint>, received ${getFriendlyDisplay(endpoint)}`);
+		}
+
+		// Serialise any provided parameters into a query string.
+		const query = new URLSearchParams(parameters).toString();
+
+		return [`${baseUrl}/${endpoint}`, query].filter(part => isNonEmptyString(part)).join("?");
+	}
+
+	/**
+	 * Retrieve the current base URL.
+	 */
+	function getBaseUrl() {
+		return baseUrl;
+	}
+
+	/**
+	 * Update the default base URL for this instance of the composable.
+	 *
+	 * @param  {string}  url
+	 *     The URL to set.
+	 */
+	function setBaseUrl(url) {
+		if (!isNonEmptyString(url)) {
+			throw new Error(`Expected non-empty string <url>, received ${getFriendlyDisplay(url)}`);
+		}
+
+		baseUrl = url;
+	}
+
 	return {
 		isLoading,
 		isReady,
 		get,
+		getBaseUrl,
+		setBaseUrl,
 	};
 }
